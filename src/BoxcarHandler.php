70c8f3266fb1e5772582489d422d3fbfdd012195
René Tønner Stokholm <zortje@gmail.com>
2015-11-22 19:23:22 +0100

    Reformatting for code style violations

--- src/BoxcarHandler.php
+++ src/BoxcarHandler.php
@@ -12,35 +12,37 @@ use Zortje\BoxcarNotifications\Notification;
  *
  * @package Zortje\MonologBoxcarHandler
  */
-class BoxcarHandler extends AbstractProcessingHandler {
+class BoxcarHandler extends AbstractProcessingHandler
+{
 
-	/**
-	 * @var Boxcar
-	 */
-	private $boxcar;
+    /**
+     * @var Boxcar
+     */
+    private $boxcar;
 
-	/**
-	 * Send push notifications to Boxcar for iOS
-	 *
-	 * @param Boxcar   $boxcar A initialized Boxcar object
-	 * @param bool|int $level  The minimum logging level at which this handler will be triggered
-	 * @param bool     $bubble Whether the messages that are handled can bubble up the stack or not
-	 */
-	public function __construct(Boxcar $boxcar, $level = Logger::DEBUG, $bubble = true) {
-		$this->boxcar = $boxcar;
+    /**
+     * Send push notifications to Boxcar for iOS
+     *
+     * @param Boxcar   $boxcar A initialized Boxcar object
+     * @param bool|int $level  The minimum logging level at which this handler will be triggered
+     * @param bool     $bubble Whether the messages that are handled can bubble up the stack or not
+     */
+    public function __construct(Boxcar $boxcar, $level = Logger::DEBUG, $bubble = true)
+    {
+        $this->boxcar = $boxcar;
 
-		parent::__construct($level, $bubble);
-	}
+        parent::__construct($level, $bubble);
+    }
 
-	/**
-	 * If title is provided we use that for the notification title, otherwise the record level name
-	 *
-	 * @inheritdoc
-	 */
-	protected function write(array $record) {
-		$title = !empty($record['context']['title']) ? $record['context']['title'] : $record['level_name'];
+    /**
+     * If title is provided we use that for the notification title, otherwise the record level name
+     *
+     * @inheritdoc
+     */
+    protected function write(array $record)
+    {
+        $title = !empty($record['context']['title']) ? $record['context']['title'] : $record['level_name'];
 
-		$this->boxcar->push(new Notification($title, $record['message']));
-	}
-
+        $this->boxcar->push(new Notification($title, $record['message']));
+    }
 }
